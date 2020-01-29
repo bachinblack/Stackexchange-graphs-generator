1cb408692aef13d7870458a3ec87e2d7eeb892df
Philippe BOUTTEREUX <philippe.bouttereux@gmail.com>
2020-01-29 15:31:57 +0100

    updated --help

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ regex = re.compile(r'((?P<years>\d+?)y)|((?P<months>\d+?)m)|((?P<days>\d+?)d)|((
 parser = argparse.ArgumentParser(description="A graphics generator for stack overflow questions.")
 
 parser.add_argument('tags', type=str, help="Tag or coma-separated list of tags to look for (ex.: c,python,javascript).")
-parser.add_argument('-f', '--fromdate', type=str, help="First date of the range.")
-parser.add_argument('-t', '--todate', type=str, help="Second date of the range.")
+parser.add_argument('-f', '--fromdate', type=str, help="First date of the range in the format 'YYYY-mm-dd[ HH]'.")
+parser.add_argument('-t', '--todate', type=str, help="Second date of the range in the format 'YYYY-mm-dd[ HH]'.")
 parser.add_argument(
     '-s', '--step', type=str,
-    help="The step in the format nt where n is a number and t a type [h, d, m, y] (ex.: Two days=2d)"
+    help="The step in the format nt where n is a number and t a type [h, d, m, y] (ex.: Two days=2d)."
 )
 parser.add_argument('-S', '--site',
     type=str, default='stackoverflow',
-    help="The stackexchange platform to look into"
+    help="The stackexchange platform to look into (default: stackoverflow)."
 )
 
 
@@ -37,7 +37,7 @@ def parse_date(sd: str):
     try:
         return datetime.strptime(sd, '%Y-%m-%d')
     except ValueError:
-        return datetime.strptime(sd, '%Y-%m-%d %H:%M:%S')
+        return datetime.strptime(sd, '%Y-%m-%d %H')
 
 if __name__ == '__main__':
     args = parser.parse_args()
